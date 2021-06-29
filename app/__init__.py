a05aad48679ab498a7c2aab59a736fe64daee4e2
Josiah <jwalutsachi4@gmail.com>
2021-06-29 19:30:55 +0300

    Added flask-login for user session management

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,14 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 
+from flask_login import LoginManager
+
 bootstrap = Bootstrap()
 db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -12,6 +18,7 @@ def create_app(config_name):
     
     bootstrap.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
     
     """ Register blueprints """
     
