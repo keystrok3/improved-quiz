184fe1c83bb8e1fdd181b59fb8e3769ddf940ee8
Josiah <jwalutsachi4@gmail.com>
2021-06-29 19:29:57 +0300

    Added abstract User model for Student and Examiner models

--- app/models.py
+++ app/models.py
@@ -11,55 +11,41 @@ registrations = db.Table('registrations',
                          db.Column('quiz_id', db.Integer, db.ForeignKey('quizes.id')))
 
 
-
-# Student model
-class Student(UserMixin, db.Model):
-    __tablename__ = "students"
-    id = db.Column(db.Integer, primary_key=True)
+# Abstract User model --- No table
+class User(UserMixin, db.Model):
+    __abstract__ = True
     email = db.Column(db.String(128), unique=True)
     fname = db.Column(db.String(64))
     lname = db.Column(db.String(64))
+    password_hash = db.Column(db.String(128))
+    
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+    
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+        
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+# Student model
+class Student(User):
+    __tablename__ = "students"
+    id = db.Column(db.Integer, primary_key=True)
     quizes = db.relationship('Quiz', 
                             secondary=registrations,
                             backref=db.backref('students', lazy='dynamic'),
                             lazy='dynamic')
     
-    password_hash = db.Column(db.String(128))
-    
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
-    
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-        
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-    
 
 # Examiner model
-class Examiner(db.Model):
+class Examiner(User):
     __tablename__ = "examiners"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(128), unique=True)
-    fname = db.Column(db.String(64))
-    lname = db.Column(db.String(64))
-    quizes = db.relationship('Quiz', backref='examiner')
+    examiner_quizes = db.relationship('Quiz', backref='examiner')
     
-    password_hash = db.Column(db.String(128))
-    
-    @property
-    def password(self):
-        raise AttributeError('password is not a readable attribute')
-    
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-        
-    def verify_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
 # Quizes
 class Quiz(db.Model):
     __tablename__ = "quizes"
