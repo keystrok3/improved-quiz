e0eafd792201f25621521ae5b7903d205515112c
Josiah <jwalutsachi4@gmail.com>
2021-06-30 22:52:31 +0300

    New forms.py in app/auth

---
+++ app/auth/forms.py
@@ -0,0 +1,26 @@
+''' WTForms form classes '''
+
+from flask_wtf import FlaskForm as Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, EqualTo, Email
+
+class RegisterForm(Form):
+    fname = StringField('First Name', validators=[DataRequired()])
+    lname = StringField('Last Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), 
+                                                EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('password2', validators=[DataRequired()])
+    role = SelectField('Role', choices=['Student', 'Examiner'])
+    submit = SubmitField('Register')
+    
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("Email already registered")
+    
+
+class LoginForm(Form):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+    
