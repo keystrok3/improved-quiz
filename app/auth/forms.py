57d10e33c102398fc1a72f3be39e65dcefb6a9f1
Josiah <jwalutsachi4@gmail.com>
2021-07-03 15:28:21 +0300

    Imported ValidationError class from exceptions.py; Changed password field validator details

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,5 +1,7 @@
 ''' WTForms form classes '''
 
+from app.models import User
+from .exceptions import ValidationError
 from flask_wtf import Form
 from wtforms import StringField, SubmitField, PasswordField, SelectField
 from wtforms.validators import DataRequired, EqualTo, Email
@@ -8,10 +10,10 @@ class RegisterForm(Form):
     fname = StringField('First Name', validators=[DataRequired()])
     lname = StringField('Last Name', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(), 
-                                                EqualTo('password2', message='Passwords must match')])
-    password2 = PasswordField('password2', validators=[DataRequired()])
-    role = SelectField('Role', choices=['Student', 'Examiner'])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('password2', validators=[DataRequired(), 
+                                                EqualTo('password', message='Passwords must match')])
+    role = SelectField('Role', choices=['STUDENT', 'EXAMINER'])
     submit = SubmitField('Register')
     
     def validate_email(self, field):
