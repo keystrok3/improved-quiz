ad371a11f83e1389f8b8daca94b8ae6b19ae3ed8
Josiah <jwalutsachi4@gmail.com>
2021-07-03 15:22:54 +0300

    Imported new classes from app/model.py

--- manage.py
+++ manage.py
@@ -1,8 +1,10 @@
 
-import os
 from app import create_app, db
+from app.models import Student, Examiner, Quiz, Question
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
+
+import os
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'development')
 
@@ -10,7 +12,7 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db)
+    return dict(app=app, db=db, Student=Student, Examiner=Examiner, Quiz=Quiz, Question=Question)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
